90c37e9dd9843482e6c5a42e65d461e95d31f71b
rosemondasilga <106432950+rosemondasilga@users.noreply.github.com>
2024-03-09 23:20:23 +0200

    Update 3-app.py

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
+
 """
-from flask_babel import Babel
+This is a basic Flask application with
+internationalization support using Flask-Babel.
+"""
+
 from flask import Flask, render_template, request
-
-
-class Config:
-    """Represents a Flask Babel configuration.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+from flask_babel import Babel
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
+
+
+# Instantiate Babel object
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
+class Config:
     """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    Configuration class for the Flask app.
+    """
+    # Define available languages
+    LANGUAGES = ['en', 'fr']
+    # Set default locale and timezone
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Use Config as config for the Flask app
+app.config.from_object(Config)
+
+# Define get_locale function using babel.localeselector decorator
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match with supported languages
+    based on request.accept_languages.
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
+def index():
+    """
+    Render the index template.
     """
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
