4df1363b20c45a6e9d054c785370a84e1a755e0b
rosemondasilga <106432950+rosemondasilga@users.noreply.github.com>
2024-03-09 23:16:43 +0200

    Update 0-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
+
 """
+This is a basic Flask application that returns
+"Hello world" when accessing the root URL ('/').
+"""
+
+# import flask module
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+# Define a route for the root URL ('/')
 
 
 @app.route('/')
-def get_index() -> str:
+def index() -> str:
     """The home/index page.
     """
     return render_template('0-index.html')
 
 
+"""run the flask app"""
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
