2472e0e173c62c6819f94847b2af20da4486b747
rosemond-mmabila <r.asilga@alustudent.com>
2024-03-28 20:17:45 +0200

    correct errors

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-      if (this.constructor !== Building
+  constructor(sqft) {
+    if (this.constructor !== Building
           && this.evacuationWarningMessage === undefined) {
-        throw Error('Class extending Building must override evacuationWarningMessage');
-      }
-      this._sqft = sqft;
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    set sqft(sqft) {
-      this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
+    this._sqft = sqft;
   }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
