a3fc6c259c346d47da9324defb48ae249ee6199b
rosemond-mmabila <r.asilga@alustudent.com>
2024-04-12 20:23:26 +0200

    updated files

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,5 @@
-module.exports = function displayMessage(stdout) {
-  console.log(stdout);
-<<<<<<< HEAD
-};
-=======
-};
->>>>>>> 912051afec2e1e97b87b2e1f59460fc296a06322
+function displayMessage(string) {
+  console.log(string);
+}
+
+module.exports = displayMessage;
